4076db89b3d0232fbe18b5af300475a232310ed3
André Michelle <andre.michelle@gmail.com>
2025-10-20 16:19:32 +0200

    cleaning

--- packages/studio/adapters/src/project/ProjectDecoder.ts
+++ packages/studio/adapters/src/project/ProjectDecoder.ts
@@ -8,7 +8,7 @@ import {
     TimelineBox,
     UserInterfaceBox
 } from "@opendaw/studio-boxes"
-import {asInstanceOf, assert, ByteArrayInput, isDefined, Option, panic, UUID} from "@opendaw/lib-std"
+import {asInstanceOf, assert, ByteArrayInput, isDefined, Maybe, Option, panic} from "@opendaw/lib-std"
 import {ProjectSkeleton} from "./ProjectSkeleton"
 import {ProjectMandatoryBoxes} from "./ProjectMandatoryBoxes"
 
@@ -25,45 +25,25 @@ export namespace ProjectDecoder {
         input.readBytes(boxGraphChunk)
         const boxGraph = new BoxGraph<BoxIO.TypeMap>(Option.wrap(BoxIO.create))
         boxGraph.fromArrayBuffer(boxGraphChunk.buffer)
-        return {boxGraph, mandatoryBoxes: readMandatoryBoxes(boxGraph, input)}
+        return {boxGraph, mandatoryBoxes: findMandatoryBoxes(boxGraph)}
     }
 
     export const findMandatoryBoxes = (boxGraph: BoxGraph): ProjectMandatoryBoxes => {
-        for (const box of boxGraph.boxes()) {
-            const found = box.accept<BoxVisitor<ProjectMandatoryBoxes>>({
-                visitRootBox: (rootBox: RootBox) => {
-                    const primaryAudioOutputUnit = asInstanceOf(rootBox.outputDevice.pointerHub.incoming().at(0)?.box, AudioUnitBox)
-                    const primaryAudioBus = asInstanceOf(primaryAudioOutputUnit.input.pointerHub.incoming().at(0)?.box, AudioBusBox)
-                    const timelineBox = asInstanceOf(rootBox.timeline.targetVertex.unwrap("TimelineBox not found").box, TimelineBox)
-                    const userInterfaceBoxes = rootBox.users.pointerHub.incoming().map(({box}) => asInstanceOf(box, UserInterfaceBox))
-                    return {
-                        rootBox,
-                        primaryAudioBus,
-                        primaryAudioOutputUnit,
-                        timelineBox,
-                        userInterfaceBoxes
-                    }
-                }
-            })
-            if (isDefined(found)) {
-                return found
+        const rootBox: Maybe<RootBox> = boxGraph.boxes().find(box =>
+            box.accept<BoxVisitor<boolean>>({visitRootBox: () => true})) as Maybe<RootBox>
+        if (isDefined(rootBox)) {
+            const primaryAudioOutputUnit = asInstanceOf(rootBox.outputDevice.pointerHub.incoming().at(0)?.box, AudioUnitBox)
+            const primaryAudioBus = asInstanceOf(primaryAudioOutputUnit.input.pointerHub.incoming().at(0)?.box, AudioBusBox)
+            const timelineBox = asInstanceOf(rootBox.timeline.targetVertex.unwrap("TimelineBox not found").box, TimelineBox)
+            const userInterfaceBoxes = rootBox.users.pointerHub.incoming().map(({box}) => asInstanceOf(box, UserInterfaceBox))
+            return {
+                rootBox,
+                primaryAudioBus,
+                primaryAudioOutputUnit,
+                timelineBox,
+                userInterfaceBoxes
             }
         }
         return panic("Could not find mandatory boxes")
-    }
-
-    const readMandatoryBoxes = (boxGraph: BoxGraph, input: ByteArrayInput): ProjectMandatoryBoxes => {
-        const rootBox = boxGraph.findBox(UUID.fromDataInput(input)).unwrap("RootBox not found") as RootBox
-        const userInterfaceBox = boxGraph.findBox(UUID.fromDataInput(input)).unwrap("UserInterfaceBox not found") as UserInterfaceBox
-        const masterBusBox = boxGraph.findBox(UUID.fromDataInput(input)).unwrap("AudioBusBox not found") as AudioBusBox
-        const masterAudioUnit = boxGraph.findBox(UUID.fromDataInput(input)).unwrap("AudioUnitBox not found") as AudioUnitBox
-        const timelineBox = boxGraph.findBox(UUID.fromDataInput(input)).unwrap("TimelineBox not found") as TimelineBox
-        return {
-            rootBox,
-            userInterfaceBoxes: [userInterfaceBox],
-            primaryAudioBus: masterBusBox,
-            primaryAudioOutputUnit: masterAudioUnit,
-            timelineBox
-        }
     }
 }
