96e7b9d176ea4c0ecdeabc9e029f99626c62f3db
André Michelle <andre.michelle@gmail.com>
2025-10-30 09:10:23 +0100

    Exclude Playfield default preset from SDK

--- packages/studio/core/src/InstrumentFactories.ts
+++ packages/studio/core/src/InstrumentFactories.ts
@@ -10,7 +10,7 @@ import {
     TapeDeviceBox,
     VaporisateurDeviceBox
 } from "@opendaw/studio-boxes"
-import {UUID} from "@opendaw/lib-std"
+import {byte, isDefined, UUID} from "@opendaw/lib-std"
 import {Waveform} from "@opendaw/lib-dsp"
 import {BoxGraph, Field} from "@opendaw/lib-box"
 import {IconSymbol, TrackType} from "@opendaw/studio-adapters"
@@ -66,7 +66,15 @@ export namespace InstrumentFactories {
         }
     }
 
-    export const Playfield: InstrumentFactory = {
+    export type PlayfieldAttachment = ReadonlyArray<{
+        note: byte
+        uuid: UUID.Bytes
+        name: string
+        durationInSeconds: number
+        exclude: boolean
+    }>
+
+    export const Playfield: InstrumentFactory<PlayfieldAttachment> = {
         defaultName: "Playfield",
         defaultIcon: IconSymbol.Playfield,
         description: "Drum computer",
@@ -75,32 +83,23 @@ export namespace InstrumentFactories {
                  host: Field<Pointers.InstrumentHost | Pointers.AudioOutput>,
                  name: string,
                  icon: IconSymbol,
-                 _attachment?: never): PlayfieldDeviceBox => {
+                 attachment?: PlayfieldAttachment): PlayfieldDeviceBox => {
             const deviceBox = PlayfieldDeviceBox.create(boxGraph, UUID.generate(), box => {
                 box.label.setValue(name)
                 box.icon.setValue(IconSymbol.toName(icon))
                 box.host.refer(host)
             })
-            const files = [
-                useAudioFile(boxGraph, UUID.parse("8bb2c6e8-9a6d-4d32-b7ec-1263594ef367"), "909 Bassdrum", 0.509),
-                useAudioFile(boxGraph, UUID.parse("0017fa18-a5eb-4d9d-b6f2-e2ddd30a3010"), "909 Snare", 0.235),
-                useAudioFile(boxGraph, UUID.parse("28d14cb9-1dc6-4193-9dd7-4e881f25f520"), "909 Low Tom", 0.509),
-                useAudioFile(boxGraph, UUID.parse("21f92306-d6e7-446c-a34b-b79620acfefc"), "909 Mid Tom", 0.385),
-                useAudioFile(boxGraph, UUID.parse("ad503883-8a72-46ab-a05b-a84149953e17"), "909 High Tom", 0.511),
-                useAudioFile(boxGraph, UUID.parse("cfee850b-7658-4d08-9e3b-79d196188504"), "909 Rimshot", 0.150),
-                useAudioFile(boxGraph, UUID.parse("32a6f36f-06eb-4b84-bb57-5f51103eb9e6"), "909 Clap", 0.507),
-                useAudioFile(boxGraph, UUID.parse("e0ac4b39-23fb-4a56-841d-c9e0ff440cab"), "909 Closed Hat", 0.154),
-                useAudioFile(boxGraph, UUID.parse("51c5eea4-391c-4743-896a-859692ec1105"), "909 Open Hat", 0.502),
-                useAudioFile(boxGraph, UUID.parse("42a56ff6-89b6-4f2e-8a66-5a41d316f4cb"), "909 Crash", 1.055),
-                useAudioFile(boxGraph, UUID.parse("87cde966-b799-4efc-a994-069e703478d3"), "909 Ride", 1.720)
-            ]
-            const samples = files.map((file, index) => PlayfieldSampleBox.create(boxGraph, UUID.generate(), box => {
-                box.device.refer(deviceBox.samples)
-                box.file.refer(file)
-                box.index.setValue(60 + index)
-            }))
-            samples[7].exclude.setValue(true)
-            samples[8].exclude.setValue(true)
+            if (isDefined(attachment)) {
+                attachment.filter(({note, uuid, name, durationInSeconds, exclude}) => {
+                    const fileBox = useAudioFile(boxGraph, uuid, name, durationInSeconds)
+                    PlayfieldSampleBox.create(boxGraph, UUID.generate(), box => {
+                        box.device.refer(deviceBox.samples)
+                        box.file.refer(fileBox)
+                        box.index.setValue(note)
+                        box.exclude.setValue(exclude)
+                    })
+                })
+            }
             return deviceBox
         }
     }
