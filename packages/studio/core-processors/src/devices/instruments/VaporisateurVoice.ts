2734d42c733d576dfe69c7116c6b56af1f50c380
André Michelle <andre.michelle@gmail.com>
2025-11-02 15:17:25 +0100

    cleaning

--- packages/studio/core-processors/src/devices/instruments/VaporisateurVoice.ts
+++ packages/studio/core-processors/src/devices/instruments/VaporisateurVoice.ts
@@ -19,7 +19,7 @@ import {VaporisateurDeviceProcessor} from "./VaporisateurDeviceProcessor"
 export class VaporisateurVoice implements Voice {
     readonly device: VaporisateurDeviceProcessor
     readonly osc: BandLimitedOscillator
-    readonly buffer: Float32Array
+    readonly oscBuffer: Float32Array
     readonly filterCoeff: BiquadCoeff
     readonly filterProcessor: BiquadMono
     readonly env: Adsr
@@ -38,14 +38,14 @@ export class VaporisateurVoice implements Voice {
         this.device = device
 
         this.osc = new BandLimitedOscillator(sampleRate)
-        this.buffer = new Float32Array(RenderQuantum)
+        this.oscBuffer = new Float32Array(RenderQuantum)
+        this.envBuffer = new Float32Array(RenderQuantum)
+        this.freqBuffer = new Float32Array(RenderQuantum)
         this.filterCoeff = new BiquadCoeff()
         this.filterProcessor = new BiquadMono()
         this.env = new Adsr(sampleRate)
         this.env.set(this.device.env_attack, this.device.env_decay, this.device.env_sustain, this.device.env_release)
         this.env.gateOn()
-        this.envBuffer = new Float32Array(RenderQuantum)
-        this.freqBuffer = new Float32Array(RenderQuantum)
         this.glide = new Glide()
         this.gainSmooth = new Smooth(0.003, sampleRate)
     }
@@ -73,20 +73,20 @@ export class VaporisateurVoice implements Voice {
         const waveform = this.device.osc_waveform
         const cutoffMapping = this.device.adapter.namedParameter.cutoff.valueMapping
         const cutoffBase = cutoffMapping.x(this.device.flt_cutoff)
+        const invSampleRate = 1.0 / sampleRate
         const resonance = this.device.flt_resonance
         const filterEnvelope = this.device.flt_env_amount
-        const outL = output.getChannel(0)
-        const outR = output.getChannel(1)
+        const [gainL, gainR] = StereoMatrix.panningToGains(this.panning, StereoMatrix.Mixing.Linear)
+        const [outL, outR] = output.channels()
 
         this.glide.process(this.freqBuffer, bpm, fromIndex, toIndex)
-        this.osc.generateFromFrequencies(this.buffer, this.freqBuffer, waveform, fromIndex, toIndex)
+        this.osc.generateFromFrequencies(this.oscBuffer, this.freqBuffer, waveform, fromIndex, toIndex)
         this.env.process(this.envBuffer, fromIndex, toIndex)
-        const [gainL, gainR] = StereoMatrix.panningToGains(this.panning, StereoMatrix.Mixing.Linear)
         for (let i = fromIndex; i < toIndex; i++) {
             const vca = this.gainSmooth.process(this.envBuffer[i] * gain)
             const cutoff = cutoffMapping.y(clamp(cutoffBase + this.envBuffer[i] * filterEnvelope, 0.0, 1.0))
-            this.filterCoeff.setLowpassParams(cutoff / sampleRate, resonance)
-            const amp = this.filterProcessor.processFrame(this.filterCoeff, this.buffer[i]) * vca
+            this.filterCoeff.setLowpassParams(cutoff * invSampleRate, resonance)
+            const amp = this.filterProcessor.processFrame(this.filterCoeff, this.oscBuffer[i]) * vca
             outL[i] += amp * gainL
             outR[i] += amp * gainR
             if (this.env.complete && this.gainSmooth.value < 1e-6) {return true}
