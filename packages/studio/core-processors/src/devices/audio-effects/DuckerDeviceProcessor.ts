7c60d2fe3a1146e71d27d913a0c530cc0395ba96
André Michelle <andre.michelle@gmail.com>
2025-11-03 08:42:18 +0100

    stereo

--- packages/studio/core-processors/src/devices/audio-effects/DuckerDeviceProcessor.ts
+++ packages/studio/core-processors/src/devices/audio-effects/DuckerDeviceProcessor.ts
@@ -1,14 +1,14 @@
 import {int, Option, Terminable, UUID} from "@opendaw/lib-std"
 import {DuckerDeviceBoxAdapter} from "@opendaw/studio-adapters"
 import {EngineContext} from "../../EngineContext"
-import {ProcessInfo, Processor} from "../../processing"
-import {AbstractProcessor} from "../../AbstractProcessor"
+import {Block, Processor} from "../../processing"
 import {PeakBroadcaster} from "../../PeakBroadcaster"
 import {AutomatableParameter} from "../../AutomatableParameter"
 import {AudioEffectDeviceProcessor} from "../../AudioEffectDeviceProcessor"
-import {AudioBuffer, RenderQuantum} from "@opendaw/lib-dsp"
+import {AudioBuffer, DuckerComputer, PPQN, Smooth} from "@opendaw/lib-dsp"
+import {AudioProcessor} from "../../AudioProcessor"
 
-export class DuckerDeviceProcessor extends AbstractProcessor implements AudioEffectDeviceProcessor {
+export class DuckerDeviceProcessor extends AudioProcessor implements AudioEffectDeviceProcessor {
     static ID: int = 0 | 0
 
     readonly #id: int = DuckerDeviceProcessor.ID++
@@ -16,8 +16,19 @@ export class DuckerDeviceProcessor extends AbstractProcessor implements AudioEff
     readonly #adapter: DuckerDeviceBoxAdapter
     readonly #output: AudioBuffer
     readonly #peaks: PeakBroadcaster
+    readonly #computer: DuckerComputer
+    readonly #smoothGainL: Smooth
+    readonly #smoothGainR: Smooth
+
+    readonly #pDepth: AutomatableParameter<number>
+    readonly #pSlope: AutomatableParameter<number>
+    readonly #pSymmetry: AutomatableParameter<number>
+    readonly #pOffset: AutomatableParameter<number>
+    readonly #pChannelOffset: AutomatableParameter<number>
 
     #source: Option<AudioBuffer> = Option.None
+
+    #needsUpdate: boolean = true
 
     constructor(context: EngineContext, adapter: DuckerDeviceBoxAdapter) {
         super(context)
@@ -25,14 +36,27 @@ export class DuckerDeviceProcessor extends AbstractProcessor implements AudioEff
         this.#adapter = adapter
         this.#output = new AudioBuffer()
         this.#peaks = this.own(new PeakBroadcaster(context.broadcaster, adapter.address))
+        this.#computer = new DuckerComputer()
+        this.#smoothGainL = new Smooth(0.003, sampleRate)
+        this.#smoothGainR = new Smooth(0.003, sampleRate)
+
+        const {depth, slope, symmetry, offset, channelOffset} = adapter.namedParameter
+
+        this.#pDepth = this.bindParameter(depth)
+        this.#pSlope = this.bindParameter(slope)
+        this.#pSymmetry = this.bindParameter(symmetry)
+        this.#pOffset = this.bindParameter(offset)
+        this.#pChannelOffset = this.bindParameter(channelOffset)
 
         this.own(context.registerProcessor(this))
+        this.readAllParameters()
     }
 
     get incoming(): Processor {return this}
     get outgoing(): Processor {return this}
 
     reset(): void {
+        this.#needsUpdate = true
         this.#peaks.clear()
         this.#output.clear()
         this.eventInput.clear()
@@ -50,19 +74,38 @@ export class DuckerDeviceProcessor extends AbstractProcessor implements AudioEff
     index(): int {return this.#adapter.indexField.getValue()}
     adapter(): DuckerDeviceBoxAdapter {return this.#adapter}
 
-    process(_processInfo: ProcessInfo): void {
+    processAudio({p0, s0, bpm}: Block, fromIndex: int, toIndex: int): void {
         if (this.#source.isEmpty()) {return}
         const input = this.#source.unwrap()
         const [inpL, inpR] = input.channels()
         const [outL, outR] = this.#output.channels()
-        for (let i = 0; i < RenderQuantum; i++) {
-            outL[i] = inpL[i]
-            outR[i] = inpR[i]
+
+        if (this.#needsUpdate) {
+            this.#computer.set(this.#pDepth.getValue(), this.#pSlope.getValue(), this.#pSymmetry.getValue())
+            this.#needsUpdate = false
+        }
+
+        const delta = PPQN.samplesToPulses(1, bpm, sampleRate)
+
+        const offset = this.#pOffset.getValue()
+        const channelOffset = this.#pChannelOffset.getValue()
+
+        for (let i = fromIndex; i < toIndex; i++) {
+            const pL = (p0 + offset + i * delta) / PPQN.Quarter
+            const gL = this.#computer.compute(pL - Math.floor(pL))
+            const pR = (p0 + offset + channelOffset + i * delta) / PPQN.Quarter
+            const gR = this.#computer.compute(pR - Math.floor(pR))
+            outL[i] = inpL[i] * this.#smoothGainL.process(gL)
+            outR[i] = inpR[i] * this.#smoothGainR.process(gR)
         }
         this.#peaks.process(outL, outR)
     }
 
-    parameterChanged(_parameter: AutomatableParameter): void {}
+    parameterChanged(parameter: AutomatableParameter): void {
+        if (parameter === this.#pDepth || parameter === this.#pSlope || parameter === this.#pSymmetry) {
+            this.#needsUpdate = true
+        }
+    }
 
     toString(): string {return `{${this.constructor.name} (${this.#id})`}
 }
