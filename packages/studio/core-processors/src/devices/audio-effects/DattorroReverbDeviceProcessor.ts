179f85f5efffbd89fcfd696020a7389382c2759c
André Michelle <andre.michelle@gmail.com>
2025-11-18 17:56:22 +0100

    adds DattorroReverbDsp

--- packages/studio/core-processors/src/devices/audio-effects/DattorroReverbDeviceProcessor.ts
+++ packages/studio/core-processors/src/devices/audio-effects/DattorroReverbDeviceProcessor.ts
@@ -1,12 +1,13 @@
 import {AudioEffectDeviceAdapter, DattorroReverbDeviceBoxAdapter} from "@opendaw/studio-adapters"
 import {int, Option, Terminable, UUID} from "@opendaw/lib-std"
-import {AudioBuffer, Event} from "@opendaw/lib-dsp"
+import {AudioBuffer, Event, StereoMatrix} from "@opendaw/lib-dsp"
 import {EngineContext} from "../../EngineContext"
 import {Block, Processor} from "../../processing"
 import {PeakBroadcaster} from "../../PeakBroadcaster"
 import {AudioProcessor} from "../../AudioProcessor"
 import {AutomatableParameter} from "../../AutomatableParameter"
 import {AudioEffectDeviceProcessor} from "../../AudioEffectDeviceProcessor"
+import {DattorroReverbDsp} from "./DattorroReverbDsp"
 
 export class DattorroReverbDeviceProcessor extends AudioProcessor implements AudioEffectDeviceProcessor {
     static ID: int = 0 | 0
@@ -17,12 +18,12 @@ export class DattorroReverbDeviceProcessor extends AudioProcessor implements Aud
 
     readonly parameterPredelay: AutomatableParameter<number>
 
+    readonly #dsp: DattorroReverbDsp
+
     readonly #output: AudioBuffer
     readonly #peaks: PeakBroadcaster
 
     #source: Option<AudioBuffer> = Option.None
-
-    #updateDattorroReverbTime: boolean = true
 
     constructor(context: EngineContext, adapter: DattorroReverbDeviceBoxAdapter) {
         super(context)
@@ -30,6 +31,7 @@ export class DattorroReverbDeviceProcessor extends AudioProcessor implements Aud
         this.#adapter = adapter
         this.#output = new AudioBuffer()
         this.#peaks = this.own(new PeakBroadcaster(context.broadcaster, adapter.address))
+        this.#dsp = new DattorroReverbDsp(sampleRate)
 
         const {preDelay} = adapter.namedParameter
         this.parameterPredelay = this.own(this.bindParameter(preDelay))
@@ -42,6 +44,7 @@ export class DattorroReverbDeviceProcessor extends AudioProcessor implements Aud
     get outgoing(): Processor {return this}
 
     reset(): void {
+        this.#dsp.reset()
         this.#output.clear()
         this.#peaks.clear()
         this.eventInput.clear()
@@ -63,7 +66,7 @@ export class DattorroReverbDeviceProcessor extends AudioProcessor implements Aud
     processAudio({bpm, flags}: Block, from: number, to: number): void {
         if (this.#source.isEmpty()) {return}
         const source = this.#source.unwrap()
-        // TODO DSP
+        this.#dsp.process(source.channels() as StereoMatrix.Channels, this.#output.channels() as StereoMatrix.Channels, from, to)
         this.#peaks.process(this.#output.getChannel(0), this.#output.getChannel(1), from, to)
     }
 
